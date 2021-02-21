25c1d8b1337ee7a981c17c08a4ce6db90b97a525
julytian <chenwenyi@youcheyihou.com>
2021-02-21 18:56:05 +0800

    feat✨ ：完善用户信息页面

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,15 +1,112 @@
 <template>
-  user page
+  <nav-header title="用户信息"></nav-header>
+  <section class="d-flex flex-column align-items-center userinfo">
+    <img class="userinfo-avatar" v-lazy="user.avatar_url" />
+    <p class="userinfo-name">{{ user.loginname }}</p>
+    <div class="d-flex align-items-center justify-content-around w-100">
+      <span
+        v-if="user.create_at"
+        v-text="getLastTimeStr(user.create_at, false)"
+      ></span>
+      <span v-if="user.score" class="userinfo-score"
+        >积分: {{ user.score }}</span
+      >
+    </div>
+  </section>
+  <van-tabs v-model:active="activeName" swipeable sticky>
+    <van-tab title="最近回复" name="replies">
+      <van-empty
+        v-if="user.recent_replies && user.recent_replies.length === 0"
+        description="暂无数据"
+      />
+      <template v-else>
+        <template v-for="item in user.recent_replies" :key="item.id">
+          <recent-item :item="item"></recent-item>
+        </template>
+      </template>
+    </van-tab>
+    <van-tab title="最新发布" name="topics">
+      <van-empty
+        v-if="user.recent_topics && user.recent_topics.length === 0"
+        description="暂无数据"
+      />
+      <template v-else>
+        <template v-for="item in user.recent_topics" :key="item.id">
+          <recent-item :item="item"></recent-item>
+        </template>
+      </template>
+    </van-tab>
+  </van-tabs>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted, ref } from 'vue';
+import NavHeader from '@/components/nav-header.vue';
+import { useRoute, useRouter } from 'vue-router';
+import { Toast, Tab, Tabs, Empty } from 'vant';
+import { apiGetUserInfo } from '@/api/user';
+import { getLastTimeStr } from '@/utils/util';
+import RecentItem from './recent-item.vue';
 
 export default defineComponent({
-
+  name: 'UserPage',
+  components: {
+    [Tab.name]: Tab,
+    [Tabs.name]: Tabs,
+    [Empty.name]: Empty,
+    NavHeader,
+    RecentItem,
+  },
+  methods: {
+    getLastTimeStr(time: string, friendly: boolean) {
+      return getLastTimeStr(time, friendly);
+    },
+  },
+  setup() {
+    const route = useRoute();
+    const router = useRouter();
+    const user = ref({});
+    const activeName = ref('replies');
+    onMounted(() => {
+      getUserInfo();
+    });
+    const getUserInfo = async () => {
+      const loginname = route.params?.loginname as string;
+      if (!loginname) {
+        Toast('缺少用户名参数');
+        router.replace('/');
+        return;
+      }
+      const res = await apiGetUserInfo<IUserInfo>(loginname);
+      user.value = res;
+    };
+    return {
+      activeName,
+      user,
+    };
+  },
 });
 </script>
 
 <style lang="scss" scoped>
+.userinfo {
+  background-color: $colore7;
+  height: 180px;
+  color: $color31;
+}
+.userinfo-avatar {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  margin-top: $gap * 3;
+  background-color: $light;
+}
+.userinfo-name {
+  margin-top: $gap * 2;
+  margin-bottom: $gap * 2;
+}
 
+.userinfo-score {
+  color: $color01;
+}
 </style>
---
+++ src/views/user/recent-item.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="d-flex recent">
+    <img class="recent-avatar" v-lazy="item.author.avatar_url" />
+    <div class="d-flex flex-column justify-content-between flex-1 recent-wrap">
+      <div class="text-ellipsis recent-title">{{ item.title }}</div>
+      <div class="d-flex align-items-center justify-content-between">
+        <span class="recent-loginname">{{ item.author.loginname }}</span>
+        <span class="recent-time">{{ getLastTimeStr(item.last_reply_at, true) }}</span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { getLastTimeStr } from '@/utils/util';
+
+export default defineComponent({
+  name: 'RecentItem',
+  props: {
+    item: {
+      type: Object as PropType<IUserRecent>,
+      default: {},
+      required: true,
+    }
+  },
+  methods: {
+    getLastTimeStr(time: string, friendly: boolean) {
+      return getLastTimeStr(time, friendly);
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.recent {
+  padding: $gap * 3;
+  background-color: $white;
+  &:not(:first-of-type){
+    border-top: 1px solid $light;
+  }
+}
+.recent-wrap {
+  width: 0;
+}
+.recent-avatar {
+  width: 45px;
+  height: 45px;
+  margin-right: $gap * 2;
+  border-radius: 50%;
+  background-color: $light;
+}
+.recent-title {
+  font-size: 18px;
+  font-weight: bold;
+  color: $color31;
+}
+.recent-loginname {
+  color: $color31;
+}
+.recent-time {
+  font-size: $font-info;
+  color: $color01;
+}
+</style>
