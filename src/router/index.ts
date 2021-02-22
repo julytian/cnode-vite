968ed56cf3947d6b623dcdcedb93ea6e8523446c
julytian <chenwenyi@youcheyihou.com>
2021-02-22 15:40:35 +0800

    feat✨ ：更改路由模式为hash路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import list from '../views/list/index.vue';
 
 const routes: Array<RouteRecordRaw> = [
@@ -47,7 +47,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(''),
+  history: createWebHashHistory(''),
   routes,
 });
 
