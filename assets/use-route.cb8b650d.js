4b782b78792c3c1bd2d2c9b660bfb3c011747025
julytian <971176857@163.com>
2021-02-23 09:46:33 +0000

    Deploying to gh-pages from  @ 513dba70dd8912496c99e53f27d76205bad44cd9 🚀

---
+++ assets/use-route.cb8b650d.js
@@ -0,0 +1,1 @@
+import{y as r}from"./index.20c60412.js";var o={to:[String,Object],url:String,replace:Boolean};function a(r){var o=r.$router,a=r.to,e=r.url,t=r.replace;a&&o?o[t?"replace":"push"](a):e&&(t?location.replace(e):location.href=e)}function e(){var o=r().proxy;return function(){a(o)}}export{o as a,a as r,e as u};
