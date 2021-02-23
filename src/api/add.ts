2648a54ebb29b12b4b5ed1bf249c4da927c0bd6f
julytian <992113471@qq.com>
2021-02-24 01:30:04 +0800

    feat✨ 新增编辑主题接口

--- src/api/add.ts
+++ src/api/add.ts
@@ -5,8 +5,19 @@ import axios from './axios';
  * @param accesstoken 用户的 accessToken
  * @param title 标题
  * @param tab 目前有 ask share job dev
- * @param content 主体内容
+ * @param content 主题内容
  */
 export function apiAddTopics<T>(params: IAddTopicParams) {
   return axios.post<T, T>('/topics', params);
+}
+/**
+ *  编辑主题
+ * @param accesstoken 用户的 accessToken
+ * @param title 标题
+ * @param tab 目前有 ask share job dev
+ * @param content 主题内容
+ * @param topic_id 主题id
+ */
+export function apiUpdateTopics<T>(params: IAddTopicParams) {
+  return axios.post<T, T>('/topics/update', params);
 }
