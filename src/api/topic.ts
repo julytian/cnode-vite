c58c3660b766bb07115c6b5dc7f030e484ee7d5b
julytian <chenwenyi@youcheyihou.com>
2021-02-23 18:45:45 +0800

    feat✨ 主题详情页新增收藏和取消收藏主题接口

--- src/api/topic.ts
+++ src/api/topic.ts
@@ -1,3 +1,5 @@
+import { IGlobalState } from '@/store';
+import { useStore } from 'vuex';
 import axios from './axios';
 
 /**
@@ -5,7 +7,13 @@ import axios from './axios';
  * @param id 主题id
  */
 export function apiGetTopicDetail<T>(id: string) {
-  return axios.get<T, T>(`/topic/${id}`);
+  const store = useStore<IGlobalState>();
+  const token = store.state.user.userInfo.token;
+  let url = `/topic/${id}`;
+  if (token) {
+    url += `?accesstoken=${token}`;
+  }
+  return axios.get<T, T>(url);
 }
 
 /**
@@ -24,9 +32,27 @@ export function apiAddTopicReplies<T>(params: IAddTopicRepliesParams) {
 
 /**
  * 为评论点赞
- * @param accesstoken 主题accesstoken
- * @param reply_id 主题reply_id
+ * @param accesstoken 用户accesstoken
+ * @param reply_id 主题id
  */
 export function apiHandleReplyUps<T>(accesstoken: string, replyId: string) {
   return axios.post<T, T>(`/reply/${replyId}/ups`, { accesstoken });
+}
+
+/**
+ * 收藏主题
+ * @param accesstoken 用户accesstoken
+ * @param topic_id 主题id
+ */
+export function apiAddTopicCollect<T>(accesstoken: string, topic_id: string) {
+  return axios.post<T, T>('/topic_collect/collect', { accesstoken, topic_id });
+}
+
+/**
+ * 取消主题
+ * @param accesstoken 用户accesstoken
+ * @param topic_id 主题id
+ */
+export function apiDelTopicCollect<T>(accesstoken: string, topic_id: string) {
+  return axios.post<T, T>('/topic_collect/de_collect', { accesstoken, topic_id });
 }
