3b6bea633d856c5117a88218eae50122502ebcc8
julytian <chenwenyi@youcheyihou.com>
2021-02-23 09:47:55 +0800

    feat✨ 主题列表新增头部标题

--- src/views/list/index.vue
+++ src/views/list/index.vue
@@ -1,13 +1,16 @@
 <template>
-  <nav-header></nav-header>
+  <nav-header :title="title"></nav-header>
   <topic-list></topic-list>
   <back-top></back-top>
 </template>
 
 <script lang="ts">
-import { defineComponent, defineAsyncComponent } from 'vue';
+import { defineComponent, defineAsyncComponent, computed } from 'vue';
 import NavHeader from '@/components/nav-header.vue'
 import TopicList from './topic-list.vue'
+import { tabs } from '@/utils/constant';
+import { useStore } from 'vuex';
+import { IGlobalState } from '@/store';
 
 export default defineComponent({
   name: 'ListPage',
@@ -15,6 +18,13 @@ export default defineComponent({
     NavHeader,
     TopicList,
     BackTop: defineAsyncComponent(() => import(/* webpackChunkName: "backtop" */ '@/components/back-top.vue')),
+  },
+  setup() {
+    const store = useStore<IGlobalState>()
+    const title = computed(() => tabs[store.state.topics.tab])
+    return {
+      title
+    }
   }
 });
 </script>
