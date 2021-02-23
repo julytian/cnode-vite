0f6683345775b55ca8d98125b3acc63ac8db506c
julytian <chenwenyi@youcheyihou.com>
2021-02-23 17:54:59 +0800

    feat✨ 新增用户收藏的主题接口

--- src/api/user.ts
+++ src/api/user.ts
@@ -14,3 +14,11 @@ export function apiGetAccessToken<T>(accesstoken: string) {
 export function apiGetUserInfo<T>(loginname: string) {
   return axios.get<T, T>(`/user/${loginname}`);
 }
+
+/**
+ * 获取用户所收藏的主题
+ * @param loginname 用户的 loginname
+ */
+export function apiGetTopicCollect<T>(loginname: string) {
+  return axios.get<T, T>(`/topic_collect/${loginname}`);
+}
